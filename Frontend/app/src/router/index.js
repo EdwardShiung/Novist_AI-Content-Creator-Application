be24d7d0d2abaf48d936b92e3d5daa597875f97e
EdwardShiung <edwardshiung@gmail.com>
2023-04-22 23:47:13 -0400

    Created Not Found Page

--- Frontend/app/src/router/index.js
+++ Frontend/app/src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../views/Index'
+import NotFound from '../views/404'
 // import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path:'*',
+    name:'/404',
+    component: NotFound,
+  },
   {
     path: '/',
     redirect: '/index',
---
+++ Frontend/app/src/views/404.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="notfound">
+        Not Found
+    </div>
+</template>
+
+<style scoped>
+
+.notfound{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+</style>
