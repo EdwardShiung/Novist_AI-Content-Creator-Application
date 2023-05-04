d4c4ac8d83a628100faca9a70f4786d408c93c82
EdwardShiung <edwardshiung@gmail.com>
2023-05-04 17:55:18 -0400

    Setting Navigation Guard

--- Frontend/app/src/router/index.js
+++ Frontend/app/src/router/index.js
@@ -51,4 +51,14 @@ const router = new VueRouter({
   routes
 })
 
+//Navigation Gard
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.eleToken ? true : false;
+   if (to.path == "/login" || to.path == "/register") {
+     next();
+   } else {
+     isLogin ? next() : next("/login");
+   }
+ })
+
 export default router
